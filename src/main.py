286824578aa99613b9131cd24a3d91c76ced5c4c
Nikita-Musk <nik52119q@gmail.com>
2024-04-07 19:43:18 +0300

    add docstring to function main() modified imports and call function prepare_operations()

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 import os
-from utils import load_data, format_operations
+from utils import load_data, prepare_operations
 from config import ROOT_DIR
 
 
 def main():
+    """
+    Функция осуществляет загрузку данных о банковских операциях из файла JSON,
+    используя функцию load_data из модуля utils.
+    Затем данные обрабатываются функцией prepare_operations,
+    чтобы получить отформатированную информацию о последних выполненных операциях.
+    """
     DATA_PATH = os.path.join(ROOT_DIR, 'src', 'operations.json')
     operations = load_data(DATA_PATH)
-    print(format_operations(operations[0]))
+    print(prepare_operations(operations))
 
 
 if __name__ == "__main__":
