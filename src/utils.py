9ae4133968a701c40092c38a802b2f1e5d356de3
Nikita-Musk <nik52119q@gmail.com>
2024-04-07 16:48:43 +0300

    Add get correct date function and change load data docstring

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,17 @@ import json
 def load_data():
     """
     Выводит в список информацию о транзакциях файла "operations.json"
-    :return:
     """
     with open("operations.json") as file:
         operations = json.load(file)
         return operations
+
+
+def get_correct_date(operations):
+    """
+    Выводит дату в правильном формате ДД.ММ.ГГГГ
+    """
+    date = operations["date"].split("T")[0]
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
 
